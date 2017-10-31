9467fe51a9f80b64c4a5b1ac696b3fa1e288221b
Pedro Madruga <pedro@occam.ooo>
2017-10-31 20:10:54 +0100

    fix: routes

--- src/services/navigation/routes.js
+++ src/services/navigation/routes.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.PUBLIC_URL;
 
-export const ROUTE_ROOT = baseUrl + '/';
-export const ROUTE_PROJECTS = baseUrl + '/projects';
-export const ROUTE_ABOUT = baseUrl + '/about';
+export const ROUTE_ROOT = `${baseUrl}/`;
+export const ROUTE_PROJECTS = `${baseUrl}/projects`;
+export const ROUTE_ABOUT = `${baseUrl}/about`;
